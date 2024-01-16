e7201ea8d0712f16ac7bfcf1f92badd22ac016bd
osasiyamu <140278554+osasiyamu@users.noreply.github.com>
2024-01-15 22:57:01 -0500

    Complete database setup

--- pgAdminDatabase.js
+++ pgAdminDatabase.js
@@ -1,20 +1,28 @@
 const { Client } = require('pg')
 
 const client = new Client({
-    host: "personalportfolio.c7q2q6o0q4j5.ca-central-1.rds.amazonaws.com",
-    user: "ppAdmin",
+    host: "searchmeup.c7q2q6o0q4j5.ca-central-1.rds.amazonaws.com",
+    user: "",
     port: 5432,
-    password: "personalportfolio",
-    database: "personalportfolio"
+    password: "",
+    database: "",
+    ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
 })
 
 client.connect();
 
 
-// client.query(`Select * from users`, (err, res)=>{
-//     if(!err){
-//         console.log(res.rows);
-//     }else{
-//         console.log(err.message);
-//     }
-// })
+
+
+
+
+client.query(`Select * from users`, (err, res)=>{
+    if(!err){
+        console.log(res.rows);
+    }else{
+        console.log(err.message);
+    }
+})
