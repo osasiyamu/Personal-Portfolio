76766f523519f94c4ade222185d2b8be25fbe094
Osamudiamen Nwoko <carlosnwoko123@gmail.com>
2024-02-03 15:16:09 -0500

    sql update

--- src/assets/sql/setup.sql
+++ src/assets/sql/setup.sql
@@ -2,7 +2,7 @@ CREATE TABLE Users (
     UserID          SERIAL,
     Username        VARCHAR(100) NOT NULL,
     PasswordHash    VARCHAR(255) NOT NULL,
-    Email           VARCHAR(255) UNIQUE.
+    Email           VARCHAR(255) UNIQUE,
     PRIMARY KEY (UserID)
 );
 
@@ -23,8 +23,8 @@ CREATE TABLE Contact (
     ProfileID           INTEGER,
     Email               VARCHAR(255),
     PhoneNumber         VARCHAR(30),
-    LinkedInProfile     VARCHAR(255);
-    Website             VARCHAR(500);
+    LinkedInProfile     VARCHAR(255),
+    Website             VARCHAR(500),
     PRIMARY KEY (ContactID),
     FOREIGN KEY (ProfileID) REFERENCES Profiles(ProfileID)
 );
@@ -94,11 +94,11 @@ CREATE TABLE JobPostings (
     PostedDate              DATE NOT NULL,
     ApplicationDeadline     DATE NOT NULL,
     CompanyName             VARCHAR(100) NOT NULL,
-    Location                Address,
+    Location                VARCHAR(100) NOT NULL,
     SalaryRange             VARCHAR(100) NOT NULL,
     Category                VARCHAR(30) CHECK (Category IN ('Software Development', 'Design & UI/UX', 'Data Analysis & Science', 'IT & Networking','Other')),
     PRIMARY KEY (JobID),
-    FOREIGN KEY (PostedByUserID) REFERS TO Users(UserID)
+    FOREIGN KEY (PostedByUserID) REFERENCES Users(UserID)
 );
 
 -- Questions
