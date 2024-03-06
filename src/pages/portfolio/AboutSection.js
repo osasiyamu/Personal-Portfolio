fdcfd4d973ef5f1828ec5853358e460bb15fd2b7
Osamudiamen Nwoko <carlosnwoko123@gmail.com>
2024-03-05 20:36:09 -0500

    About section

--- src/pages/portfolio/AboutSection.js
+++ src/pages/portfolio/AboutSection.js
@@ -1,8 +1,32 @@
-const AboutSection = ({profileInfo}) => {
+import { useEffect, useState } from 'react';
+
+const AboutSection = ({profileId}) => {
+
+    const [dataInfo, setDataInfo] = useState([]);
+
+    const getDataInfo = () => {
+        fetch(`http://localhost:5555/myportfolio/about/${profileId}`)
+		.then(response => {
+			if (!response.ok) {
+			  	throw new Error('Network response was not ok');
+			}
+			return response.json();
+		})
+        .then(data => {
+            setDataInfo(data);
+        })
+		.catch(error => {
+			console.error("Error fetching data: ", error);
+		});
+    };
+
+    useEffect(() => {
+		getDataInfo();
+    });
 
     return (
         <div>
-            <p>{profileInfo}</p>
+            <p>{dataInfo["about"]}</p>
         </div>
     );
 }
--- src/pages/portfolio/SectionContent.js
+++ src/pages/portfolio/SectionContent.js
@@ -4,8 +4,6 @@ const SectionContent = ({ profileId }) => {
 
     return (
         <div>
-            <p>Content for {window.location.pathname}</p>
-
             {(window.location.pathname === "/myportfolio") && 
                 <AboutSection profileId={profileId} />
             }
